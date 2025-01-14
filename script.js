fa5719b03c4005beb77ec1bf432cf9ac69994305
Liandro8 <liandromagalhaesking91@gmail.com>
2025-01-14 02:52:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Obrigado por entrar em contato! Responderemos em breve.');
+});
